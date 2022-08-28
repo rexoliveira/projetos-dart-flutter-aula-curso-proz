718fb5ddd1f0b686edc6a9954471504ca4e35c08
rexoliveira <roolivei@yahoo.com.br>
2022-08-27 23:58:15 -0300

    Ajustes

--- aula8_24082022/lista_de_maps/test/testa_funcao.dart
+++ aula8_24082022/lista_de_maps/test/testa_funcao.dart
@@ -1,9 +1,8 @@
-/* import 'dart:convert';
-
+import 'dart:convert';
 import 'dart:io';
 
 List minhaMusicas = <Map<String, dynamic>>[];
-void main() {
+Future<void> main() async {
   print("\x1B[2J\x1B[0;0H");
   List<String> propriedades = <String>[
     'nome',
@@ -15,7 +14,7 @@ void main() {
     "anoLancamento" */
   ];
   Map<String, dynamic> temp = {};
-  
+
   for (var i = 0; i < 1; i++) {
     for (var element in propriedades) {
       print("Digite um $element:");
@@ -28,12 +27,7 @@ void main() {
   print(minhaMusicas);
 
   print("===================================");
-  //updateKey('nome', 'pedro');
-  /* print(minhaMusicas[0].update('nome', (value) => 'Pedro'));
+  
+  print(minhaMusicas);
 
-  print(minhaMusicas); */
-minhaMusicas[0].update('key1', (value) => '');
-
-print(minhaMusicas);
-} */
-
+}
