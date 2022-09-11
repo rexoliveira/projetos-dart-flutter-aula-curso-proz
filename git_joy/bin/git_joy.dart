ff8ef96e8d80748d06d8f2a727191ebf9e1e5802
rexoliveira <roolivei@yahoo.com.br>
2022-09-11 17:24:53 -0300

    Nova funcionalidade

--- git_joy/bin/git_joy.dart
+++ git_joy/bin/git_joy.dart
@@ -1,5 +1,5 @@
 import 'package:git_joy/git_joy.dart' as git_joy;
 
 void main(List<String> arguments) {
-  print('Hello world: ${git_joy.calculate()}!');
+  print('Noa funcionalidade ${git_joy.calculate()}!');
 }
